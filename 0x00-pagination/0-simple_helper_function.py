9d9c555d70a97951561a280242b04f31d792cd72
xeno <xeno@xeno.0.2.15>
2024-05-06 17:51:30 +0100

    pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"pagination help function"
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    "retrieves index"
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
