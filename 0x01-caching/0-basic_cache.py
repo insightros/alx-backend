821b93f4eff9fb355b4b4af86ba644b0984763a9
xeno <xeno@xeno.0.2.15>
2024-05-08 16:28:43 +0100

    caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,10 +4,13 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    "represents object"
     def put(self, key, item):
+        "adds key"
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        "retrieves key"
         return self.cache_data.get(key, None)
