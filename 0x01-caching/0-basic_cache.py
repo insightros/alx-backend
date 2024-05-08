83be90bd67c359789cc4c9b94e324b6e0031c39e
xeno <xeno@xeno.0.2.15>
2024-05-08 16:27:37 +0100

    caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"basic chaching"
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        return self.cache_data.get(key, None)
